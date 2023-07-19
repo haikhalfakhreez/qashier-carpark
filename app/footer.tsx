4b28b018bde3c678bf59beeecbfcbb8d48a39a16
Haikhal Fakhreez <ekaliacid@Ekal-MacBook-Pro.local>
2023-07-19 19:45:13 +0800

    update GitHub link

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="text-gray-400 text-xs flex items-center justify-center py-6">
       <a
-        href="https://github.com/haikhalfakhreez/qashier-carpark-information"
+        href="https://github.com/haikhalfakhreez/qashier-carpark"
         target="_blank"
         rel="noopener noreferrer"
         className="hover:text-black transition-colors"
